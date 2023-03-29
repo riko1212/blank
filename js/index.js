a7f6307e0c1b82afcb69e0ced55dd36fc9393370
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-29 11:13:51 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,10 +6,27 @@
  * Виведення даних: console.log та alert. Вкладка Console в Chrome Devtools.
  */
 
+// console.log('Hello world');
+// alert(10);
+
 /*
  * Змінні: оголошення з let та const.
  */
 
+// const num = 10;
+
+// console.log(num);
+
+// num = 20;
+
+// console.log(num);
+
 /*
  * Отримання даних: prompt та confirm.
  */
+
+// const userAge = prompt('Ваш вік?');
+// const isAdult = confirm('Вам є 18?');
+
+// console.log(userAge);
+// console.log(isAdult);
