a86f4013ca123dcbb2607d5e3bd4c42d715c0b24
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-31 17:35:51 +0300

    +++

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+// import num, { add } from './math';
+
+// const user = {
+//   firstName: 'Hilda',
+//   lastName: 'Steele',
+//   age: 40,
+
+//   getFirstName() {
+//     return this.firstName;
+//   },
+// };
+
+// const jsonUser = JSON.stringify(user);
+
+// console.log(jsonUser);
+// console.log(JSON.parse(jsonUser));
+
+// console.log('Hello');
+// try {
+//   const data = JSON.parse('Well, this is awkward');
+// } catch (err) {
+//   console.log(err);
+// }
+// console.log("❌ You won't see this log");
+
+// const user = {
+//   fistName: 'Bertie',
+//   lastName: 'Sherman',
+//   age: 50,
+// };
+
+// localStorage.setItem('userData', JSON.stringify(user));
+
+// console.log(JSON.parse(localStorage.getItem('userData')));
